41c2a3778999d3fb499380c832492e6be0497a9a
Sushil <skshinde2028@gmail.com>
2023-05-24 15:42:33 +0530

    [Sushil] Validating with the annotation

---
+++ Annotations/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Annotations
+{
+    internal class Employee
+    {
+        [Required(ErrorMessage = "Employee {0} is required")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage =("Name should contain at least two letters and maximum 100"))]
+        [DataType(DataType.Text)]
+        public string name { get; set; }
+
+        [Range(18,99, ErrorMessage = ("Age should be between 18 and 99"))]
+        public int age { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string phoneNumber { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string email { get; set; }
+    }
+}
---
+++ Annotations/Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Annotations
+{
+    internal class Test
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Employee class validation");
+
+            Employee employee = new Employee();
+            employee.name = "Sushil";           
+            employee.age = 23;                                                                                                            
+            employee.phoneNumber = "91 9902141561";
+            employee.email = "sksg@mail.com";
+
+            ValidationContext context = new ValidationContext(employee, null, null);  //(object, service, dictionaries)
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(employee, context, results, true);
+
+            if(!valid)
+            {
+                foreach(ValidationResult result in results)
+                {
+                    Console.Write($"Member name: {result.MemberNames}");
+                    Console.WriteLine($"ErrorMessage : {result.ErrorMessage}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Name: {employee.name} \n Age: {employee.age} \n PhoneNumber: {employee.phoneNumber} \n Email: {employee.email} ");
+
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
